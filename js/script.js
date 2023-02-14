078f7f29f1880e9d3f1976af315909e3dc714118
OlegNestayko <oleg_nestayko@ukr.net>
2023-02-14 17:38:23 +0200

    Added burger-menu.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const iconBurger = document.querySelector('.header-burger');
+const menu = document.querySelector('.header__list');
+const closeMenu = document.querySelector('.close');
+iconBurger.addEventListener('click', function (e) {
+  menu.classList.toggle('active');
+  closeMenu.classList.toggle('active');
+});
